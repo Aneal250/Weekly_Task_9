990818f1948d0d7786fe0181a70693bb1fa122b9
oroxis <orowo.039@gmail.com>
2021-08-03 22:32:30 +0100

    Added Orowo functional component

---
+++ src/Orowo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Orowo() {
+    return (
+        <div>
+            <h1>Orowo Okelezo</h1>
+            <h3>Revenue Collection Estimator</h3>
+            <h2>0148</h2>
+        </div>
+    )
+}
