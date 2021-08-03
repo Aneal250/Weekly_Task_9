5110d3b5e03d5f48a43764f80cac4a8a22aad80e
sammydavid <sammydavid215@gmail.com>
2021-08-03 15:45:50 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyProfile from './components/Ogechukwu';
 function App() {
+  const Profile = {
+    Name: 'Ogechukwu Okoh',
+    Project: 'Instagram Cloning',
+    Sit: 33
+  }
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
+      <MyProfile Okoh = {Profile}/>
+      <h1>Adding My Profile Information</h1>
+      
     </div>
   );
 }
