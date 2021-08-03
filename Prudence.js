a097658770e0241e2d6605b5ccfe64288a635c4f
Prudencial20 <85343963+Prudencial20@users.noreply.github.com>
2021-08-03 16:08:08 +0100

    Add files via upload

---
+++ Prudence.js
@@ -0,0 +1,19 @@
+import React from 'react'
+ 
+function Prudence() {
+    const myName = 'EKI PRUDENCE'
+    let projectGroup = 'News App'
+    let seatNumber = '41'
+
+     return (
+         <div>
+            <h1>{myName}</h1>
+            <h3>{projectGroup}</h3>
+            <h2>{seatNumber}</h2>
+         </div>
+     );
+ }
+ 
+
+
+export default Prudence
