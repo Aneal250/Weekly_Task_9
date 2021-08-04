45a4d3ddbc897cc4442c86bf25ffb6d8621f4ea9
winnerneo <obarowinner@gmail.com>
2021-08-04 15:45:00 -0400

    Functional component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Winner from  './components/Winner'
 
 function App() {
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
+      <Winner/>
     </div>
   );
 }
