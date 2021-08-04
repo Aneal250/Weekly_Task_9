78b44ba3b0a26c612e414c8afb69ab738e0484c4
kenojason <jasonogodo@gmail.com>
2021-08-04 16:34:01 -0700

    contribution

---
+++ src/Keno/page.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Name: Ogodo Oghenekeno</h1>
+            <h3>Project group: Group 6 (Revenue collection estimator)</h3>
+            <h2>Seat No: 17</h2>
+        </div>
+    )
+}
