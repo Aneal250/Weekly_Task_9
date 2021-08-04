c2215ffbfe5bfb92c76524c5086ab993e13f6457
UruAnn <85343197+UruAnn@users.noreply.github.com>
2021-08-04 13:09:18 +0100

    Cloned Repo

---
+++ src/Elemele.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Elemele() {
+    return (
+        <div>
+            <h1>Name: {'Elemele Ann'}</h1>
+            <h3>Project Group: {9}</h3>
+            <h2>Seat Number: {9}</h2>
+
+        </div>
+    )
+}
