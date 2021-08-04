1bbeb6be50799967a34ce318a4a6dfe1489455ff
nyerovwo24 <erherha4rukeme@gmail.com>
2021-08-04 16:23:57 +0100

    Contributed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+       
+        <div>
+            <h1>{'OGHENERUKEME ERHERHA'}</h1>
+            <h3>Group: {12}</h3>
+            <h2>Seat Number: {36}</h2>
+        </div>
+       
+    )
+}
