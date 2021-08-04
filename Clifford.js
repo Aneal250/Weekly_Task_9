be2772a52050eacd5ee36463490cb2a56745246d
cliffordovo <86098916+cliffordovo@users.noreply.github.com>
2021-08-04 12:39:39 +0100

    Done and Dusted

---
+++ Clifford.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Clifford() {
+    return (
+        <div>
+            <h1>Name: {'Ogheneovo Clifford'}</h1>
+            <h3>Project Group: {1}</h3>
+            <h2>Seat Number: {1}</h2>
+        </div>
+    )
+}
