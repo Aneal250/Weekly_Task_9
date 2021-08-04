78d3735a131f2c299c709776e615f3148af280e0
honourzy <85343080+honourzy@users.noreply.github.com>
2021-08-04 04:22:25 -0700

    my files

---
+++ src/Component/Chukwudebe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Chukwudebe() {
+    const Name = 'Chukwudebe Leslie'
+    const projectGroup = 'House Rental Service' 
+    const seatNumber = 'No 3'
+
+    return (
+        <div className="Chukwudebe">
+            <h1>{Name}</h1>
+            <h3>{projectGroup}</h3>
+            <h2>{ seatNumber}</h2>
+        </div>
+    )
+}
