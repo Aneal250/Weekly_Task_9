99b4fc108dcaa1b19df65472627284d489fadf72
Harrison <ebiharrison5@gmail.com>
2021-08-04 11:25:09 -0700

    Weekly Task 9 for Atesponu Ayeoritseneaso - 30

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
+      <h1>Atesponu Ayeoritseneaso</h1>
+      <h2>Emergency Reporting - 10</h2>
+      <h3>Seat No: 30</h3>
     </div>
   );
 }
