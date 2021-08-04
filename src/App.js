0076b0adde78d9369440f361f34b633c2ec31c1d
maryann878 <maryannnwagor@gmail.com>
2021-08-04 22:44:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Project from './components/MaryAnn';
 
 function App() {
+  const Contribution = {
+    name: 'MaryAnn Nwagor',
+    project: 'Instagram Clone',
+    set: 'Group 11'
+  }
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
+            <h1><b>Project Capstone Details</b></h1>
+
+      <Project details = {Contribution}/>
     </div>
   );
 }
