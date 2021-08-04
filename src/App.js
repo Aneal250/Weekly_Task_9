8a286532f3e68eada25b97d4aad8581a3fcf746e
JP-Edagwhare <jonnie.whiztech@gmail.com>
2021-08-04 15:29:45 +0100

    Contribution from John Peter (26)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import JohnPeter from './Components/JohnPeter';
 
 function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <JohnPeter />
     </div>
   );
 }
