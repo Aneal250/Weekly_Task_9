ccb5abfbd2209416099a223f09f937fdecde2558
Palevu4real <85342861+Palevu4real@users.noreply.github.com>
2021-08-04 02:45:19 -0700

    added some css and js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Palevu from './Component/Palevu';
 
 function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <Palevu />
     </div>
   );
 }
