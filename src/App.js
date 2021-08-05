77671bb9fa9be53392357cbd8660080f268215b9
brian0864 <abugewabrian@gmail.com>
2021-08-05 09:42:02 +0100

    my clone comitt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Clone from './Component/Clone';
 
 function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <Clone />
     </div>
   );
 }
