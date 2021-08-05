3c1b5ed9ec5869b72c88d4d76c8ebf28eeb4f74f
joyous-hub <jetanyobere@gmail.com>
2021-08-05 10:55:44 +0100

    Added my name and seat number

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <h1>Iyere Joy Etanyobere Oborakpororo</h1>
+      <h2>FindMyBlood</h2>
+      <h3>22</h3>
+
     </div>
   );
 }
