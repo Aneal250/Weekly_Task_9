61845cd28e0428e7ddf78e72b6f81a4b77032d83
maygoldlove <85343054+maygoldlove@users.noreply.github.com>
2021-08-04 23:38:18 -0700

    added all required files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Josephine from './Component/Josephine';
 
 function App() {
   return (
     <div className="App">
       <h1>Add your own Contribution</h1>
+      <Josephine />
     </div>
   );
 }
