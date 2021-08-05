1c4a460f34cc55ad4f038b1b9afc03616b8687f8
Your Name <jessydaniella2014@yahoo.com>
2021-08-05 05:11:43 -0700

    Jess Commit This is my contribution

---
+++ src/Jess.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+const Jess = () => {
+    return(
+        <div>
+            <h1>Ohen Jessica</h1>
+            <h2>13</h2>
+            <h3>Find my blood</h3>
+        </div>
+
+    )
+
+}
+export default Jess;
+
+
