0548a471b48e61a91502b3f31cbed985194551f2
silvaihub <aghoghoza@gmail.com>
2021-08-05 09:47:16 +0100

    created a new component and staged all my changes. i also removed an <h1> tag

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Button() {
+    return (
+        <div>
+            <h1> Name:Okposusu Aghogho Silva</h1>
+            <h3> Group 4: Job Listing</h3>
+            <h2>Sit No: 10</h2>
+        </div>
+    )
+}
