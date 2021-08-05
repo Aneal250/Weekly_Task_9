303d90a76ee11410c08b1101deb0aeb4e25a6bc5
chukkyTech <85343162+chukkyTech@users.noreply.github.com>
2021-08-05 02:38:00 +0100

    updated Project.js

--- src/Components/ProjectData.js
+++ src/Components/ProjectData.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ProjectData(props) {
     return(
 <div> 
-    <h1>Name: {props. myProject.name}</h1>
+    <h1>First Name: {props.myProject.name}</h1>
     <h3>Project Group: {props.myProject.group}</h3>
     <h2>Seat Number: {props.myProject.seat}</h2>
 </div>
