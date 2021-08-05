5d726ef70ce0a504a0ae1e64506db32aa8bba3dc
brian0864 <abugewabrian@gmail.com>
2021-08-05 09:47:31 +0100

    clone succesful

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Clone from './Component/Clone';
 function App() {
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
+    
       <Clone />
     </div>
   );
---
+++ src/Component/Clone.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Clone() {
+    return (
+        <div>
+            <h1>{"Abugewa Brian"}</h1>
+            <h3>Group: {4}</h3>
+            <h2>Seat Number: {43}</h2>
+        </div>
+    )
+}
