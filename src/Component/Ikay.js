ffa1e19796247314249c7b4c3e21ed19e2e0e4de
ikay00406 <ik_okaforc@yahoo.com>
2021-08-05 09:23:00 +0100

    WeeklyTask9

---
+++ src/Component/Ikay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Ikay() {
+    return (
+        <div>
+            <h1>Ikechukwu Okafor</h1>
+            <h3>Group 11 - Instagram Clone</h3>
+            <h2>Seat Number 35</h2>
+        </div>
+    )
+}
