73c203e87f9a807ba95755a54140e327137e6669
Elohor-Grace <omorogunelohor@gmail.com>
2021-08-05 11:12:14 +0100

    first commit into weekly task 9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Elohor from './Elohor'
-
 function App() {
   return (
     <div className="App">
-      <h1>Add your own Contribution</h1>
-      <h1>Omorogun Elohor Grace</h1>
-       <h3>Inventory Management System(8)</h3>
-        <h2>23</h2>  
-     <Elohor />
+       <h1>Add your own Contribution</h1>
+       <h1>Omorogun Elohor Grace</h1>
+       <h3>Inventory Management System</h3>
+       <h3>23</h3>
+       <Elohor />
+     
     </div>
   );
 }
