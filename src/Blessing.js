b20e4cba1115b0b3db84a9f2984ca0b4d2dd79e2
Lordgracefoundme <87809808+Lordgracefoundme@users.noreply.github.com>
2021-08-05 13:44:02 +0100

    Blessing Pull Request

---
+++ src/Blessing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Blessing() {
+    return (
+        <div>
+            <h1>OGUEH BLESSING</h1>
+            <h3>GROUP 10 (Emergency Reporting)</h3>
+            <h2>Seat 12</h2>
+        </div>
+    )
+}
